e6c18a85598bdf7d5350d7b82233bb48076aee61
vincent-mashupgarage <vincent@mashupgarage.com>
2025-08-31 12:38:25 +0800

    fix cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000' # Your Next.js app URL
+      origins 'http://localhost:3001', 'http://localhost:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3000'
   
       resource '*',
         headers: :any,
